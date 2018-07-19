06433c709c8b9e763eeedf1c580bf1ef10178cfc
FishSaidNo <FishSaidNo@users.noreply.github.com>
2018-07-19 10:46:06 +1000

    Updated to work with Meteor 1.7

--- aggregate.js
+++ aggregate.js
@@ -12,7 +12,7 @@ const defaultOptions = ({
   ...options
 });
 
-export const ReactiveAggregate = function (subscription, collection, pipeline = [], options = {}) {
+ReactiveAggregate = function (subscription, collection, pipeline = [], options = {}) {
   // fill out default options
   const {
     observeSelector, observeOptions, delay, lookupCollections, clientCollection
--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: "jcbernack:reactive-aggregate",
-  version: "1.0.0",
+  name: 'jcbernack:reactive-aggregate',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: "Reactively publish aggregations.",
   // URL to the Git repository containing the source code for this package.
-  git: "https://github.com/JcBernack/meteor-reactive-aggregate",
+  git: 'https://github.com/JcBernack/meteor-reactive-aggregate',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: "README.md"
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom("1.5");
-  api.use(['ecmascript', 'underscore', 'mongo']);
+  api.versionsFrom('1.5');
+  api.use(['ecmascript', 'underscore', 'mongo', 'sakulstra:aggregate@1.4.3'], ['server']);
 
-  api.addFiles("./mongo-collection-aggregate.js");
-  api.mainModule("./aggregate.js");
+  api.export('ReactiveAggregate', 'server');
+  api.mainModule('aggregate.js', 'server');
 });
